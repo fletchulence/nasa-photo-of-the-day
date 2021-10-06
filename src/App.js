ea31806d2b9086a002b707e43423e15d2a00bcab
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-10-06 10:48:50 -0700

    getting stuff back,, need components though

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import {BASE_URL, API_KEY} from './constants/index'
 
-function App() {
+function App(props) {
+
+  const [ data, setData ] = useState()
+
+  useEffect(() => {
+    function getData() {
+      axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data)
+        setData(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    }
+    getData()
+  }, [])
+
   return (
     <div className="App">
-      <p>
+        {
+          data &&
+        <>
+        <div>
+          <p>{data.date}</p>
+          <div className='imgContainer'>
+            <img src={data.url} href='${data.url}'/>
+            <h1> {data.title} </h1>
+          </div>
+        </div>
+        </>
+        }
+      <p> 
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = '08RUTpZUVoh48AzzqtPaWTMxQGBJvRyVZSLGB6yF'
+
+//https://api.nasa.gov/planetary/apod?api_key=08RUTpZUVoh48AzzqtPaWTMxQGBJvRyVZSLGB6yF
+
+//{base_url}?api_key={api-key}
